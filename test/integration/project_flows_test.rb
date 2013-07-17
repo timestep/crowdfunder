94a04ece630cb5a261c5d63faf45c101af5c59ff
timestep <jbosshellokitty@gmail.com>
2013-07-17 17:14:45 -0400

    added test for navigation

--- test/integration/project_flows_test.rb
+++ test/integration/project_flows_test.rb
@@ -24,4 +24,20 @@ class ProjectFlowsTest < ActionDispatch::IntegrationTest
 		assert page.has_content?('Project 3')
 	end
 
+	test 'navigation' do
+		#visit the root Url
+		visit '/'
+		#assert the page we're on is root
+		assert_equal root_path, current_path
+		#assert the home nav elemen tis active
+		assert_equal "Home", find('.navbar ul li.active a').text
+
+		#click on link to projects
+		find('.navbar ul').click_link('Projects')
+
+		#assert the page we're on is the projects page
+		assert_equal projects_path, current_path
+		#assert the projects nav element is active
+		assert_equal "Projects", find('.navbar ul li.active a').text
+	end
 end
