bc4927105bb4967e19e86b037753b1baff3f32a4
timestep <jbosshellokitty@gmail.com>
2013-07-17 17:56:17 -0400

    writing test for project show pages

--- test/integration/project_flows_test.rb
+++ test/integration/project_flows_test.rb
@@ -22,6 +22,13 @@ class ProjectFlowsTest < ActionDispatch::IntegrationTest
 		assert page.has_content?('Project 1')
 		assert page.has_content?('Project 2')
 		assert page.has_content?('Project 3')
+
+    # Click a link to the project1's show page
+		click_link 'Project 1'
+		 # Assert we're on project1's show page
+		assert_equal project_path(project1), current_path
+    # Assert on this page the first h1 has the text project1's title
+		assert find('h1:first').has_content? project1.title
 	end
 
 	test 'navigation' do
