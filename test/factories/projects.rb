34ad6a24f87a016db195c5862a34191292dda008
timestep <jbosshellokitty@gmail.com>
2013-07-17 13:12:07 -0400

    created project factory girl

--- test/factories/projects.rb
+++ test/factories/projects.rb
@@ -2,5 +2,10 @@
 
 FactoryGirl.define do
   factory :project do
+  	user
+  	title 'Rearden Metal'
+  	teaser "Jesus Based Metal; Lighter than air, stronger than diamonds."
+  	description "It Color is undescriable in the english vocalulary."
+  	goal 1000000
   end
 end
