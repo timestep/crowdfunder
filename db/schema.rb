c5aefe09548b88dcb497bbd92277edc209ca8f00
timestep <jbosshellokitty@gmail.com>
2013-07-17 17:07:04 -0400

    recommited

--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20130717164850) do
   create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
-    # t.string   "username",         null: false
+    t.string   "username",         null: false
     t.string   "email"
     t.string   "crypted_password"
     t.string   "salt"
