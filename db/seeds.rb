a708d23de41a2e5ff6761043ddd762fd1a824290
timestep <jbosshellokitty@gmail.com>
2013-07-17 12:59:46 -0400

    added seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy.all
+Project.destroy.all
+
+user = User.create!(first_name: "John", last_name: "Doe", email: "john@doe.com", password: "password")
+
+project1 = user.projects.create!(title: "Project 1",
+	teaser:"Teaser text 1", description: "Description 1",
+	goal: 13000)
+
+project2 = user.projects.create!(title: "Project 2",
+	teaser:"Teaser text 2", description: "Description 2", 
+	goal: 210000)
+
+projects3	= user.projects.create!(title: "Project 3",
+	teaser:"Teaser text 2", description: "Description 3",
+	goal: 30000)
