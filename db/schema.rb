bb367307e47e43db0a59998a2e588a3c80685619
timestep <jbosshellokitty@gmail.com>
2013-07-17 12:50:18 -0400

    added migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130717164623) do
+ActiveRecord::Schema.define(version: 20130717164850) do
+
+  create_table "projects", force: true do |t|
+    t.string   "title"
+    t.string   "teaser"
+    t.integer  "user_id"
+    t.text     "description"
+    t.integer  "goal"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "projects", ["user_id"], name: "index_projects_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "first_name"
