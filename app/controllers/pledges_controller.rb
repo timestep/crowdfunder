82625062e1efe32d9f11915c8fe3d1d9a456f1aa
timestep <jbosshellokitty@gmail.com>
2013-07-18 17:55:52 -0400

    added form for pledge

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -4,9 +4,18 @@ class PledgesController < ApplicationController
 	before_filter :require_project
 
 	def new
+		@pledge = @project.pledges.build
 	end
 
 	def create
+		@pledge = @project.pledges.build params[:pledge]
+		@pledge.user = current_user
+
+		if @pledge.save
+			redirect_to @project, notice: "hi"
+		else
+			render :new
+		end
 	end
 
 	private
