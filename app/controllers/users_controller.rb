fbd65cfb85bc54424fe9427f33ab936c5ae03a13
timestep <jbosshellokitty@gmail.com>
2013-07-18 14:38:20 -0400

    added alert test for failed userreg;

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   		auto_login(@user)
   		redirect_to root_url, :notice => "Account created"
   	else
+      flash.now.alert = 'Try again'
   		render :new
   	end
   end
--- test/integration/user_authentication_flows_test.rb
+++ test/integration/user_authentication_flows_test.rb
@@ -38,6 +38,8 @@ class UserAuthenticationFlowsTest < ActionDispatch::IntegrationTest
     assert_equal users_path, current_path
     # ... no message saying "Account created" should appear
     assert page.has_no_content?("Account created")
+      # Should see "Try again" message on failure to register 
+    assert find('.alert:first').has_content?("Try again")
   end
 
 
