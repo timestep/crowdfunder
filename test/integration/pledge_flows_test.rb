4f41f86bc8f7aa237c9562e7f3d946dfdb4d1f1d
timestep <jbosshellokitty@gmail.com>
2013-07-18 17:10:31 -0400

    added test for pledge flows

--- test/integration/pledge_flows_test.rb
+++ test/integration/pledge_flows_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class PledgeFlowsTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+ 	
+ 	setup do 	
+ 		@project = FactoryGirl.create :project
+ 	end
+
+ 	test 'requires authenticated user' do	
+ 		visit project_path(@project)
+
+ 		click_link 'Back This Project'
+
+ 		assert_equal new_session_path, current_path
+ 		assert page.has_content?('Please login first.')
+ 	end
+
+ 	test 'authenticated user can pledge valid amount' do
+ 		user = setup_signed_in_user
+
+ 		visit project_path(@project)
+ 		click_link 'Back This Project'
+
+ 		assert_equal new_project_pledge_path(@project), current_path
+ 		assert_equal 0, Pledge.count
+
+ 		fill_in 'pledge[amount]', with: 100
+ 		click_button 'Pledge Now'
+
+ 		assert_equal project_path(@project), current_path
+ 		assert page.has_content?("Thanks for pledging")
+
+ 		assert pledge = Pledge.order(:id).last
+ 		assert_equal user, pledge.user
+ 		assert_equal @project, pledge.project
+ 		assert_equal 100, pledge.amount
+ 	end
 end
