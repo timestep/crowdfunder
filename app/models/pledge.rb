54e394731ba8509e1bbd90938e28377bd38260ad
timestep <jbosshellokitty@gmail.com>
2013-07-18 16:55:20 -0400

    modified test and passing some of them with validations

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,4 +1,8 @@
 class Pledge < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
+
+  validates_presence_of :user
+  validates_presence_of :project
+
 end
--- test/models/pledge_test.rb
+++ test/models/pledge_test.rb
@@ -4,35 +4,35 @@ class PledgeTest < ActiveSupport::TestCase
 	test 'requires a user' do
 		pledge = FactoryGirl.build(:pledge, user:nil)
 
-		assert_false pledge.save
-		assert_equal "can't be blank", pledge,errors[:user].first
+		refute pledge.save
+		assert_equal "can't be blank", pledge.errors[:user].first
 	end
 
 	test 'requres a project' do
 		pledge = FactoryGirl.build(:pledge, project:nil)
 
-		assert_false pledge.save
+		refute pledge.save
 		assert_equal "can't be blank", pledge.errors[:project].first	
 	end
 
 	test 'requires amount' do
 		pledge = FactoryGirl.build(:pledge, amount:nil)
 
-		assert_false pledge.save	
+		refute pledge.save	
 		assert_equal "is not a number", pledge.errors[:amount].first
 	end
 
 	test 'require amount greater than 0' do
 		pledge = FactoryGirl.build(:pledge, amount: -10)
 
-		assert_false pledge.save
+		refute pledge.save
 		assert_equal "must be greater than 0", pledge.errors[:amount].first
 	end
 
 	test 'requires amount to be an integer' do
 		pledge = FactoryGirl.build(:pledge, amount: 'abc')
 
-		assert_false pledge.save
+		refute pledge.save
 		assert_equal "is not a number", pledge.errors[:amount].first
 	end
 end
