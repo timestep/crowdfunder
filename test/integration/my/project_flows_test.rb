4165f081219925abe25007d400dd1d60e35ef206
timestep <jbosshellokitty@gmail.com>
2013-07-19 12:23:53 -0400

    c/p to avoid errors

--- test/integration/my/project_flows_test.rb
+++ test/integration/my/project_flows_test.rb
@@ -1,59 +1,60 @@
 require 'test_helper'
 
 class My::ProjectFlowsTest < ActionDispatch::IntegrationTest
+	
+  test "lists only my projects" do 
+    me = setup_signed_in_user
+    other_user = FactoryGirl.create :user
 
-	test 'lists only my projcets' do
-		me = setup_signed_in_user
-		other_user = FactoryGirl.create :user 
+    3.times { FactoryGirl.create :project, user: me }
+    2.times { FactoryGirl.create :project, user: other_user, title: "Other Dude's Project" }
 
-		3.times { FactoryGirl.create :project, user: me }
-		2.times { FactoryGirl.create :project, user: other_user, title: "Other Dude's Project" }
+    visit '/my/projects'
 
-		visit '/my/projects'
+    page.assert_selector('div.project', count: 3)
+    assert page.has_no_content?("Other Dude's Project")
+  end
 
-		page.assert_selector('div.project', count: 3)
-		assert page.has_no_content?("Other Dude's Project")
-	end
+  test "can edit my project" do
+    me = setup_signed_in_user
 
-	test 'can edit my project' do
-		me = setup_signed_in_user
+    project = FactoryGirl.create :project, user: me, title: "My Project"
 
-		project = FactoryGirl.create :project, user: me, title: "My Project"
+    visit edit_my_project_path(project)
 
-		visit edit_my_project_path(project)
+    fill_in 'project[title]', with: "It is really my project"
 
-		fill_in 'project[title]', with: 'It is really my project'
+    click_button 'Update Project'
 
-		click_button 'Update Project'
+    assert_equal my_projects_path, current_path
+    assert page.has_content?("It is really my project")
+  end
 
-		assert_equal my_project_path, current_path
-		assert page.has_content?("It is really my project")
-	end
+  test "cannot edit someone else's project" do
+    me = setup_signed_in_user
 
-	test "cannot edit someone else's project" do
-		me = setup_signed_in_user
+    other_user = FactoryGirl.create :user
+    project = FactoryGirl.create :project, user: other_user, title: "Other Dude's Project"
 
-		other_user = FactoryGirl.create :user
-		project = FactoryGirl.create :project, user: other_user, title: "Other Dude's Project"
+    visit edit_my_project_path(project)
 
-		visit edit_my_project_path(project)
-		assert has_content?("doesn't exsist")
-	end
+    assert has_content?("doesn't exist")
+  end
 
-	test "successful creation of project makes it public right away" do
-		me = setup_signed_in_user
+  test "successful creation of project makes it public right away" do
+    me = setup_signed_in_user
+    # If we don't specify a user, it will needlessly CREATE a new one
+    project = FactoryGirl.build(:project, user: me)
 
-		project = FactoryGirl.build(:project, user: me)
+    visit 'my/projects/new'
 
-		visit 'my/projects/new'
+    fill_in 'project[title]', with: "Test Project"
+    fill_in 'project[teaser]', with: "Test Project Teaser"
+    fill_in 'project[description]', with: "Test Project Description"
+    fill_in 'project[goal]', with: "12000"
 
-		fill_in 'project[title]', with: "Test Project"
-		fill_in 'project[teaser]', with: "Test Project Teaser"
-		fill_in 'project[description]', with: "Test Project Description"
-		fill_in 'project[goal]', with: "12000"
+    click_button 'Publish Project'
 
-		click_button 'Publish Project'
-
-		assert_equal my_projects_path, current_path
-	end
+    assert_equal my_projects_path, current_path
+  end
 end
