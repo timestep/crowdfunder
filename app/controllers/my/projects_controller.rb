ac495cb79b3c9ba3ff5b2272d32ea4fb5433cb3c
timestep <jbosshellokitty@gmail.com>
2013-07-19 15:48:16 -0400

    filled in methods

--- app/controllers/my/projects_controller.rb
+++ app/controllers/my/projects_controller.rb
@@ -8,15 +8,28 @@ class My::ProjectsController < ApplicationController
 	end
 
 	def new 
+		@project = current_user.projects.build
 	end
 
 	def create
+		@project = current_user.projects.build params[:project]
+		if @project.save
+			redirect_to [:my, :projects], notice: 'Project created!'
+		else
+			render :new
+		end
 	end
 	
 	def edit
 	end	
 
 	def update
+		
+		if @project = update_attributes params[:project]
+			redirect_to [:my, :projects], notice: 'Project updated!'
+		else
+			render :edit
+		end
 	end
 
 	private
