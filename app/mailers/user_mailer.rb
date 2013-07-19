6f21c1e61602eacf54e392f03be8fde112d65bf2
timestep <jbosshellokitty@gmail.com>
2013-07-19 17:55:57 -0400

    sending pledge to project owner

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
   default from: "from@example.com"
 
   def new_pledge(pledge)
-  	@user = pledge.user
-  	mail(to: @user.email)
+  	@user = pledge.project.user
+  	mail(to: @user.email, subject: "Your project recieved some backing on CrowdFunder." )
   end
 end
