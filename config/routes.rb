4af79de12afff663327d78c61fd2e75200941dc9
timestep <jbosshellokitty@gmail.com>
2013-07-21 15:41:17 -0400

    deletion test passing

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Crowdfunder::Application.routes.draw do
 	get "login" => "sessions#new", :as => "login"
 	get "signup" => "users#new", :as => "signup"
 
+
   resources :projects do
  		resources :pledges, only: [:new, :create]
  	end
