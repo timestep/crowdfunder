a80e2620ed177acde45c7f9637d9b909d6f22dc7
timestep <jbosshellokitty@gmail.com>
2013-07-23 14:18:40 -0400

    added carrierwave gem

--- app/controllers/my/images_controller.rb
+++ app/controllers/my/images_controller.rb
@@ -4,9 +4,18 @@ class My::ImagesController < ApplicationController
 	before_filter :require_project
 
   def index
+    @images = @project.images.all
+    @image = @project.images.build
   end
 
   def create
+    @image = @project.images.build(params_images)
+    if @image.save
+      redirect_to [:my, @project, :images], notice: "Image uploaded. Check it out below."
+    else
+      @images = @project.images.order(:id)
+      render :index
+    end
   end
 
   private
@@ -15,4 +24,7 @@ class My::ImagesController < ApplicationController
   	@project = current_user.projects.find params[:project_id]
   end
 
+  def params_images
+    params.require(:image).permit!
+  end
 end
